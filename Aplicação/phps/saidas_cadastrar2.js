c6278e38e1573bb6303dde6026100f43e6bbed9f
Mauricio Witzgall <mauwitz@icloud.com>
2016-03-15 14:59:09 -0300

    - Criado novas regras entre os campos que dão desconto ao final da venda. Ao sair de um campo o sistema auto-calcula o outro (percentual e valor)

--- Aplicação/phps/saidas_cadastrar2.js
+++ Aplicação/phps/saidas_cadastrar2.js
@@ -7,19 +7,51 @@ function desconto(campo) {
         centsSeparator: ',',
         thousandsSeparator: '.'
     });
+    
+    total2=$("#valbru").val();
+    total=$("#valbru").val();
+    total3=total.split(" ");
+    total=total3[1];
+    total=total.replace(".","");
+    total=total.replace(",",".");
+    
+    descper=$("#descper").val();
+    descper=descper.replace(".","");
+    descper=descper.replace(",",".");
+    //alert(descper);
+    
+    if ((descper>100)||(descper<0)) {
+        alert("O valor de desconto deve ser maior que 0 e menor que 100!");
+        $('#descper').val("0,00");
+        $("#total").val(total2);
+        return false;
+    }
 
-    //Recalcula o total
-    $.post("saidas_totalcomdesconto.php",{
-        valbru:$("input[name=valbru]").val(),
-        descper:$("input[name=descper]").val()
-    }, function(valor) {
-        $("input[name=total]").val("R$ "+ valor);//alimenta o input desabilitado
-        $("input[name=total2]").val("R$ "+valor);//alimenta o input hidden
+    //Zera o Desconto Valor e Dinheiro
+    $("input[name=dinheiro]").val(""); 
+    
+    //Atualiza desconto em valor conforme percentual
+    descval=total*descper/100;
+    descval=descval.toFixed(2);
+    //alert(total+"/"+descper+"/"+descval);
+    $("input[name=descval]").val(descval).priceFormat({
+        prefix: 'R$ ',
+        centsLimit: 2,
+        centsSeparator: ',',
+        thousandsSeparator: '.'
     });
     
-    //Zera o Desconto Valor e Dinheiro
-    $("input[name=descval]").val("R$ 0,00"); 
-    $("input[name=dinheiro]").val(""); 
+    
+    //Recalcula o total final com desconto
+    valorfinal=total-descval;
+    valorfinal=valorfinal.toFixed(2);
+    valorfinal=valorfinal.replace(".",",");
+    $("input[name=total]").val("R$ "+ valorfinal);//alimenta o input desabilitado
+    $("input[name=total2]").val("R$ "+valorfinal);//alimenta o input hidden
+
+    
+    
+    
 
 }
 //Campo Desconto Valor
@@ -31,6 +63,43 @@ function desconto2(campo) {
         thousandsSeparator: '.'
     });
 
+    //Atualiza percentual de desconto conforme o valor digitado
+    total2=$("#valbru").val();
+    total=$("#valbru").val();
+    total3=total.split(" ");
+    total=total3[1];
+    total=total.replace(".","");
+    total=total.replace(",",".");
+    total = parseFloat(total);
+    
+    descval=$("#descval").val();
+    descval3=descval.split(" ");
+    descval=descval3[1];
+    descval=descval.replace(".","");
+    descval=descval.replace(",",".");
+    descval = parseFloat(descval);
+
+    descper=parseFloat(descval*100/total);
+    descper=descper.toFixed(2);
+    $("input[name=descper]").val(descper).priceFormat({
+        prefix: '',
+        centsLimit: 2,
+        centsSeparator: ',',
+        thousandsSeparator: '.'
+    });
+    
+    //alert(" "+descval+" / "+total+"/"+descper);
+    
+    if (descval>total) {
+        alert("Você não pode dar um desconto com valor maior ao valor total da venda!");
+         $("input[name=descper]").val("0,00");
+         $("input[name=descval]").val("R$ 0,00");
+         $("input[name=total]").val(total2);
+         return false;
+    }
+    
+    
+
     //Recalcula o total
     $.post("saidas_totalcomdesconto2.php",{
         valbru:$("input[name=valbru]").val(),
@@ -41,9 +110,11 @@ function desconto2(campo) {
     });
     
     //Zera o Desconto Percentagem e Dinheiro
-    $("input[name=descper]").val("0"); 
-    $("input[name=dinheiro]").val("R$ 0,00");     
+    $("input[name=dinheiro]").val("");     
 }
+
+
+
 
 //Campo Dinheiro
 function mascara_dinheiro() {
@@ -54,6 +125,9 @@ function mascara_dinheiro() {
         thousandsSeparator: '.'
     });    
 }
+
+
+
 
 function recalcula_total() {
     //Recalcula o total
